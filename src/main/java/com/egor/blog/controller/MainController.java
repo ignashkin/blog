81dfe8be048bba80c1437b466ddb38ed821f17ab
ignashkin <ignat.stam@gmail.com>
2019-07-31 11:40:48 +0300

    optimization import

--- src/main/java/com/egor/blog/controller/MainController.java
+++ src/main/java/com/egor/blog/controller/MainController.java
@@ -1,8 +1,7 @@
 package com.egor.blog.controller;
+
 import com.egor.blog.domain.Post;
-import com.egor.blog.domain.User;
 import com.egor.blog.repo.PostRepository;
-import com.egor.blog.repo.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
